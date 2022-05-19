677191a36b45d385accd3cab1d0cf427dbdb2fca
Jaelson Pereira <86059311+jaelps@users.noreply.github.com>
2022-05-19 09:37:19 -0300

    Update listaDeTarefas.js
    add script para incluir função para novo iten a lista

--- listaDeTarefas.js
+++ listaDeTarefas.js
@@ -1,8 +1,21 @@
-const novaTarefa = document.querySelector('[data-form-button]')
+const criarTarefa = (evento) => {
+    
+    evento.preventDefault()
 
-
-novaTarefa.addEventListener('click',() => {
+    const lista = document.querySelector('[data-list]')
     const input = document.querySelector('[data-form-input]')
     const valor = input.value
-    console.log( valor )
-})
+
+    const tarefa = document.createElement('li')
+    tarefa.classList.add('task')
+    const conteudo = `<p class="content">${valor}</p>`
+
+    tarefa.innerHTML = conteudo
+
+    lista.appendChild(tarefa)
+    input.value = " "
+}
+
+const novaTarefa = document.querySelector('[data-form-button]')
+
+novaTarefa.addEventListener('click', criarTarefa)
