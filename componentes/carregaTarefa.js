ce0d52c42194bf7b581cf449e1664d27041bfbf5
Jaelson Pereira <86059311+jaelps@users.noreply.github.com>
2022-05-23 10:06:39 -0300

    Update carregaTarefa.js
    corrigindo erros

--- componentes/carregaTarefa.js
+++ componentes/carregaTarefa.js
@@ -3,8 +3,9 @@ import { Tarefa } from "./criaTarefas.js"
 export const carregaTarefa = () => {
     const lista = document.querySelector('[data-list]')
 
-    const tarefasCadastradas = JSON.parse.localStorage.getItem('tarefas')|| []
+    const tarefasCadastradas = JSON.parse(localStorage.getItem('tarefas'))|| []
 
+    lista.innerHTML = " "
     tarefasCadastradas.forEach((tarefa) => {
         lista.appendChild(Tarefa(tarefa))
     })
